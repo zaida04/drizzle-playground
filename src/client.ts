d215f3d0c0aa3b5b196285f6ac2106b1fe284f94
nico <nico.03727+github@gmail.com>
2024-01-04 00:30:47 -0500

    chore: notice abt db url

--- src/client.ts
+++ src/client.ts
@@ -2,6 +2,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
 // this connects us to the postgres db. it's our raw connection
+// your database url should go as the first string parameter
 const sql_connection = postgres("...", { max: 1 });
 // this creates the drizzle utility so we can run queries
 const drizzle_client = drizzle(sql_connection);
